d206b70700ab6109dbde4fc72c888ea40754ad8c
Herb Segismar <herbert.segismar@yahoo.com>
2025-08-28 17:14:17 +1200

    change routing to createbrowserrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,28 @@
 // src/App.tsx
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import Devotionals from "./pages/Devotionals";
 import Bookmarks from "./pages/Bookmarks";
 import Prayers from "./pages/Prayers";
 import Settings from "./pages/Settings";
 import NotFound from "./pages/NotFound";
+import RootLayout from "./RootLayout";
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="devotionals" element={<Devotionals />} />
+        <Route path="bookmarks" element={<Bookmarks />} />
+        <Route path="prayers" element={<Prayers />} />
+        <Route path="settings" element={<Settings />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    )
+  );
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/devotionals" element={<Devotionals />} />
-            <Route path="/bookmarks" element={<Bookmarks />} />
-            <Route path="/prayers" element={<Prayers />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <RouterProvider router={router}/>
   );
 }
 
